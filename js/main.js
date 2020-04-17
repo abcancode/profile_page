606b9038b8144bf5150dd5fa23932e9d19e8c08b
Ugwu Abraham Chidozie <jonasbraham@gmail.com>
2020-04-17 23:21:17 +0100

    inital commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+$("form").submit(function(e) {
+              
+    var error = "";
+    
+    if ($("#email").val() == "") {
+        
+        error += "The email field is required.<br>"
+        
+    }
+
+    if ($("#name").val() == "") {
+        
+        error += "The name field is required.<br>"
+        
+    }
+                  
+    if ($("#phone").val() == "") {
+        
+        error += "The subject field is required.<br>"
+        
+    }
+    
+    if ($("#content").val() == "") {
+        
+        error += "The content field is required.<br>"
+        
+    }
+    
+    if (error != "") {
+        
+       $("#error").html('<div class="alert alert-danger" role="alert"><p><strong>There were error(s) in your form:</strong></p>' + error + '</div>');
+        
+        return false;
+        
+    } else {
+        
+        return true;
+        
+    }
+})
